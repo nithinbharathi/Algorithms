6b10a498e40af29ea8c1ec5f4c72870ae7eff52e
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2020-01-17 16:09:57 +0530

    Create Exponenciation.java

---
+++ Datastructure/src/Exponenciation.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Exponenciation
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		int p = input.nextInt();
+		int res1 = rec_exp(n,p);
+		int res2 = iter_exp(n,p);
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+	public static int iter_exp(int n,int p){    //Time Complexity O(N), Space Complexity = O(1)
+	    int res =1;
+	    for(int i=0;i<p;i++){
+	        res*=n;
+	    }
+	    return res;
+	}
+	public static int rec_exp(int n,int p){     //Time complexity (N), Space Complexity = O(p)
+	    if(p == 0)return 1;
+	    return n*rec_exp(n,p-1);
+	}
+}
