50484db3a15c4ffbcb3167650b3a16d6d915c46a
Nithin Bharathi <39995394+nithinbharathi@users.noreply.github.com>
2020-01-19 16:49:37 +0530

    Update SegmentedSeive.java

--- Datastructure/src/SegmentedSeive.java
+++ Datastructure/src/SegmentedSeive.java
@@ -4,29 +4,20 @@
 
 
 public class SeiveOfEratosthenes {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          
+	public static void main(String[] args) {          
           segment_seive(2,10);
 	}
 	public static void segment_seive(int low,int high){
 		int seive[] = new int[high-low+1];   //seive to mark all the multiples of a prime number between the given range
-    
-    
 		if(low>high){                 // Boundary  Condition check
 			System.out.println("enter a valid range");
 			return;
-		}		
-    
-    
-		for(int i =2;i<=Math.sqrt(high);i++){       // Goes till root of high because if a number n is not 
-                                                // divided by any other number d till 
-                                                // the square root of the number n then it is prime
-                                                
-			
-      int start = Math.max(i * i,(low+i-1)/i*i); // finding the first multiple of the prime number i in given range
+		 }		
+		for(int i =2;i<=Math.sqrt(high);i++){       // Goes till root of high because if a number n is not  divided by any other number d till 
+							    // the square root of the number n then it is prime
+			int start = Math.max(i * i,(low+i-1)/i*i); // finding the first multiple of the prime number i in given range
                                                   
-      for(int j = start;j<=high;j+=i){      // and making jumps of i to reach the next multiple of i;        
+		      for(int j = start;j<=high;j+=i){      // and making jumps of i to reach the next multiple of i;        
 				seive[j-low] = 1;
 			}
 		}
